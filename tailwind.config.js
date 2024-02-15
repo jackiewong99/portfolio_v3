1f5a73b0b9f08a9dcc46b3a7e7d1572129ea4f28
Jackie Wong <wongzjackie@gmail.com>
2024-02-14 18:24:18 -1000

    Initial commit to portfolio_v3 project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      forest: {
+        100: "#ddfddb",
+        200: "#a7d7c5",
+        300: "#80b5a0",
+        600: "#1eb178",
+        700: "#5c8d89",
+        800: "#12714d",
+        900: "#275854",
+      },
+      yellow: {
+        100: "#fdfdf8",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
