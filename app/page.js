c581fd6ac3e4484922ecfe2e0c31ad70431e59bf
Jackie Wong <wongzjackie@gmail.com>
2024-02-27 21:07:42 -1000

    Import Projects component to root component.

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Header from "@/components/Header";
 import About from "@/components/About";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Header />
       <About />
+      <Projects />
     </main>
   );
 }
