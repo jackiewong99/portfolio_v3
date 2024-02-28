db99f316f19fe593fedde043484da1c32247f919
Jackie Wong <wongzjackie@gmail.com>
2024-02-27 21:06:44 -1000

    Create projects_list.js

---
+++ assets/projects_list.js
@@ -0,0 +1,51 @@
+export const projectList = [
+  {
+    title: "Credit Karma Clone",
+    description: `A simple clone of the Credit Karma landing page with design changes and improvements.`,
+    website: `https://credit-karma-clone.vercel.app/`,
+    github_repo: `https://github.com/jackiewong99/credit-karma-clone`,
+    tech_list: ["Next.js", "Tailwind CSS", "Vercel"],
+  },
+  {
+    title: "GHG Tracker",
+    description: `A web app developed in collaboration with Hawaiian Electric Industries to allow residents of Hawai'i to track their daily greenhouse gas emissions.`,
+    website: ``,
+    github_repo: `https://github.com/hot-n-code/ghg-tracker`,
+    tech_list: ["React.js", "Meteor"],
+  },
+  {
+    title: "Portolio",
+    description: `The first iteration of my portfolio website that I designed and built in 2020. This website was built with Vue JS.`,
+    website: `https://jackiewong99.github.io/portfolio`,
+    github_repo: `https://github.com/jackiewong99/portfolio`,
+    tech_list: ["Vue.js", "Node.js", "CSS"],
+  },
+  {
+    title: "Gruhb",
+    description: `A web app that makes finding a place to eat on the UH Manoa campus an easier task. This was a group project for my Software Engineering I course.`,
+    website: ``,
+    github_repo: `https://github.com/gruhb-doc/gruhb-doc`,
+    tech_list: ["React.js", "Node.js", "Meteor"],
+  },
+  {
+    title: "Form Validation",
+    description: `Project that practices the validation of common form fields' data input. This project was my first experience building with Vue JS.`,
+    website: `https://form-validation-29df6.web.app`,
+    github_repo: `https://github.com/jackiewong99/form-validation`,
+    tech_list: ["Vue.js", "Firebase", "CSS"],
+  },
+  {
+    title: "Grub Bot",
+    description: `A web app that simplifies the task of finding a place to eat. The user answers a few questions and the chat bot randomly finds a restaurant based on the user's preference.`,
+    website: `https://jackiewong99.github.io/grub-bot`,
+    github_repo: `https://github.com/jackiewong99/grub-bot`,
+    tech_list: ["JS", "Node.js", "Yelp API", "CSS"],
+  },
+  {
+    title: "Pathfinding Visualizer",
+    description: `A simple project that currently visualizes Dijkstra's Algorithm and is hosted on GitHub Pages. This project was my first dive into React JS.`,
+    website: `https://jackiewong99.github.io/Pathfinding-Visualizer`,
+    github_repo: `https://github.com/jackiewong99/Pathfinding-Visualizer`,
+    tech_list: ["React.js", "CSS"],
+  },
+];
