c5726290201d96e3d40bcb38908720b308c636bc
Jackie Wong <wongzjackie@gmail.com>
2024-03-02 20:49:59 -1000

    Update Projects component.
    - Improve projects grid responsiveness.
    - Update alignment consistency bug for ProjectCard component.

--- components/Projects.js
+++ components/Projects.js
@@ -8,7 +8,7 @@ const Projects = () => {
           Projects
         </h1>
       </div>
-      <div className="grid w-3/5 grid-cols-1 gap-4 md:grid-cols-2 lg:w-4/5 lg:grid-cols-3 xl:w-3/5">
+      <div className="grid w-4/5 grid-cols-1 gap-4 p-5 md:grid-cols-2 lg:grid-cols-3 2xl:w-3/5">
         {projectList.map((project, index) => {
           return <ProjectCard project={project} key={index} />;
         })}
@@ -19,28 +19,29 @@ const Projects = () => {
 
 const ProjectCard = ({ project }) => {
   return (
-    <div className="bg-forest-100 flex flex-col justify-between gap-4 rounded-md p-5 shadow-xl">
+    <div className="bg-forest-100 flex flex-col gap-5 rounded-md p-5 shadow-xl">
       <div className="text-forest-700 flex justify-between">
         <h5>link</h5>
         <h5>link</h5>
       </div>
-      <div className="flex flex-col items-start justify-center gap-6 pb-10">
+      <div className="flex min-h-52 flex-col gap-5">
         <h2 className="text-forest-700 text-2xl font-semibold">
           {project.title}
         </h2>
         <h5 className="text-forest-800 text-lg">{project.description}</h5>
       </div>
-      <div>
-        <ul className="flex items-center justify-start">
-          {project.tech_list.map((tech_name, index) => {
-            return (
-              <li key={index} className="text-forest-800 text-lg opacity-60">
-                {tech_name}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <ul className="flex items-center justify-start gap-5">
+        {project.tech_list.map((tech_name, index) => {
+          return (
+            <li
+              key={index}
+              className="text-forest-800 text-base opacity-60 md:text-xs xl:text-sm min-[1643px]:text-lg"
+            >
+              {tech_name}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
