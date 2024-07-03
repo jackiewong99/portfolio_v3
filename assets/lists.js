44a2aa19652c9859f5f72d4a98c5de1bb6a21177
Jackie Wong <wongzjackie@gmail.com>
2024-07-02 16:20:14 -1000

    Update utility "lists.js" file.
    - Consolidate all lists for labels and cards into one single "lists.js" file.

---
+++ assets/lists.js
@@ -0,0 +1,70 @@
+export const navLabels = [
+  {
+    name: "About",
+    url: "about",
+  },
+  {
+    name: "Projects",
+    url: "projects",
+  },
+  {
+    name: "Contact",
+    url: "contact",
+  },
+  {
+    name: "RESUME",
+    url: "",
+  },
+];
+
+export const projectList = [
+  {
+    title: "Credit Karma Clone",
+    description: `A simple clone of the Credit Karma landing page with design changes and improvements.`,
+    website: `https://credit-karma-clone.vercel.app/`,
+    github_repo: `https://github.com/jackiewong99/credit-karma-clone`,
+    tech_list: ["Next.js", "Tailwind CSS", "Vercel"],
+  },
+  {
+    title: "GHG Tracker",
+    description: `Collaborated with Hawaiian Electric Industries to track daily greenhouse gas emissions of residents of Hawai'i.`,
+    website: `https://hot-n-code.github.io/`,
+    github_repo: `https://github.com/hot-n-code/ghg-tracker`,
+    tech_list: ["React.js", "Meteor", "Framer-Motion"],
+  },
+  {
+    title: "Portolio",
+    description: `The first iteration of my portfolio website that I designed and built in 2020.`,
+    website: `https://jackiewong99.github.io/portfolio`,
+    github_repo: `https://github.com/jackiewong99/portfolio`,
+    tech_list: ["Vue.js", "Node.js", "CSS"],
+  },
+  {
+    title: "Gruhb",
+    description: `A group project for a software engineering course that simplifies finding a place to eat on the UH Manoa campus.`,
+    website: `https://gruhb-doc.github.io/`,
+    github_repo: `https://github.com/gruhb-doc/gruhb-doc`,
+    tech_list: ["React.js", "Node.js", "Meteor"],
+  },
+  {
+    title: "Form Validation",
+    description: `Project that practices the validation of common form fields' data input.`,
+    website: `https://form-validation-29df6.web.app`,
+    github_repo: `https://github.com/jackiewong99/form-validation`,
+    tech_list: ["Vue.js", "Firebase", "CSS"],
+  },
+  {
+    title: "Grub Bot",
+    description: `A web app that simplifies the task of finding a place to eat through a chat bot and the Yelp API.`,
+    website: `https://jackiewong99.github.io/grub-bot`,
+    github_repo: `https://github.com/jackiewong99/grub-bot`,
+    tech_list: ["JS", "Node.js", "Yelp API", "CSS"],
+  },
+  {
+    title: "Pathfinding Visualizer",
+    description: `My first dive into React.js, which currently visualizes Dijkstra's Algorithm.`,
+    website: `https://jackiewong99.github.io/Pathfinding-Visualizer`,
+    github_repo: `https://github.com/jackiewong99/Pathfinding-Visualizer`,
+    tech_list: ["React.js", "CSS"],
+  },
+];
