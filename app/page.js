6d860927f1658fdcb8b4a8d357f85e084a0d6e15
Jackie Wong <wongzjackie@gmail.com>
2024-07-02 16:25:52 -1000

    Update page.js
    - Add Navbar, Contact, and Footer components.

--- app/page.js
+++ app/page.js
@@ -1,14 +1,20 @@
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
+      <Navbar />
       <Header />
       <About />
       <Projects />
+      <Contact />
+      <Footer />
     </main>
   );
 }
