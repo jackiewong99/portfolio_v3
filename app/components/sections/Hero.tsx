d2d2b26e2cdc59bae34da587b3be8509714d5f64
Jackie Wong <wongzjackie@gmail.com>
2026-09-19 00:02:52 -1000

    Create temp hero content to visualize nav

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -1,7 +1,11 @@
 export default function Hero() {
   return (
-    <div>
-      <h1 className='text-5xl font-bold font-family-serif'>Jackie Wong</h1>
+    <div
+      className='flex items-center justify-center max-w-[1120] min-h-screen'
+      data-navigation-hero
+    >
+      {/* Used by useScrollVisibility to switch the fixed navigation to its solid state. */}
+      <h1 className='font-display text-5xl font-semibold'>Jackie Wong</h1>
     </div>
   );
 }
