4cfe784c6ca32d2f25b9f61d581adf20c7492565
Jackie Wong <wongzjackie@gmail.com>
2026-09-23 07:06:45 -1000

    Add smooth scroll to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
     <html
       lang='en'
-      className={`${display.variable} ${body.variable} ${mono.variable} h-full antialiased`}
+      className={`${display.variable} ${body.variable} ${mono.variable} h-full scroll-smooth antialiased`}
     >
       <body className='min-h-full flex flex-col'>
         {/* Navigation is a client component; the layout can remain server-rendered. */}
